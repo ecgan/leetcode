f393d8bf8c00f92aef9a1040480568a4109bd86e
Gan Eng Chin <ecgan@users.noreply.github.com>
2019-08-04 10:27:59 +0800

    working solution and tests.

---
+++ problems/intersection-of-two-arrays-ii/README.md
@@ -0,0 +1,7 @@
+# Intersection of Two Arrays II
+
+LeetCode #: [350](https://leetcode.com/problems/intersection-of-two-arrays-ii/)
+
+Difficulty: Easy
+
+Topics: Hash table, two pointers, binary search, sort.
---
+++ problems/intersection-of-two-arrays-ii/solution.js
@@ -0,0 +1,34 @@
+const sortBy = require('lodash/sortBy')
+
+const sortedIntersect = (nums1, nums2) => {
+  const result = []
+  let p1 = 0
+  let p2 = 0
+
+  while (p1 < nums1.length & p2 < nums2.length) {
+    if (nums1[p1] === nums2[p2]) {
+      result.push(nums1[p1])
+      p1++
+      p2++
+    }
+
+    if (nums1[p1] > nums2[p2]) {
+      p2++
+    }
+
+    if (nums1[p1] < nums2[p2]) {
+      p1++
+    }
+  }
+
+  return result
+}
+
+const intersect = (nums1, nums2) => {
+  const sortedNums1 = sortBy(nums1)
+  const sortedNums2 = sortBy(nums2)
+
+  return sortedIntersect(sortedNums1, sortedNums2)
+}
+
+module.exports = intersect
