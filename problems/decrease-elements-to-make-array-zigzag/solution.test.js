d7d93796d0fda1eeefe361f009cf07ebd2a2b53b
Gan Eng Chin <ecgan@users.noreply.github.com>
2019-08-04 22:55:34 +0800

    rename tests.

--- problems/decrease-elements-to-make-array-zigzag/solution.test.js
+++ problems/decrease-elements-to-make-array-zigzag/solution.test.js
@@ -1,6 +1,6 @@
 const movesToMakeZigzag = require('./solution')
 
-test('Sample 1', () => {
+test('Example 1', () => {
   const nums = [1, 2, 3]
 
   const result = movesToMakeZigzag(nums)
@@ -8,7 +8,7 @@ test('Sample 1', () => {
   expect(result).toBe(2)
 })
 
-test('Sample 2', () => {
+test('Example 2', () => {
   const nums = [9, 6, 1, 6, 2]
 
   const result = movesToMakeZigzag(nums)
@@ -16,7 +16,7 @@ test('Sample 2', () => {
   expect(result).toBe(4)
 })
 
-test('Sample 2', () => {
+test('With multiple same contiguous values', () => {
   const nums = [10, 4, 4, 10, 10, 6, 2, 3]
 
   const result = movesToMakeZigzag(nums)
