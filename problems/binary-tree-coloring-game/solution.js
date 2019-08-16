f9647f95b4038d5920c310f287764f887a55a7d3
Gan Eng Chin <ecgan@users.noreply.github.com>
2019-08-17 03:39:49 +0800

    solution with performance optimization.

--- problems/binary-tree-coloring-game/solution.js
+++ problems/binary-tree-coloring-game/solution.js
@@ -17,39 +17,39 @@ const btreeGameWinningMove = (root, n, x) => {
   }
 
   const target = Math.floor(n / 2)
+  const acc = {
+    count: 0,
+    nodeX: null
+  }
 
   if (root.val === x) {
-    const count = dfsSearchCount(root.left, target, 0)
+    dfsSearchCountNodeX(root.left, target, x, acc)
 
-    if (count === target) {
+    if (acc.count === target) {
       return false
     }
 
     return true
   }
 
-  const acc = {
-    count: 0,
-    nodeX: null
-  }
+  dfsSearchCountNodeX(root, target, x, acc)
 
-  const resultPreX = dfsSearchCountWithX(root, target, x, acc)
-
-  if (resultPreX.count > target) {
+  if (acc.count > target) {
     return true
-  } else if (resultPreX.count === target) {
+  } else if (acc.count === target) {
     return false
   }
 
-  const countXLeft = dfsSearchCount(resultPreX.nodeX.left, target, 0)
+  const accLeft = { count: 0 }
+  dfsSearchCountNodeX(acc.nodeX.left, target, x, accLeft)
 
-  if (countXLeft > target) {
+  if (accLeft.count > target) {
     return true
-  } else if (countXLeft === target) {
+  } else if (accLeft.count === target) {
     return false
   }
 
-  const countXRight = n - resultPreX.count - countXLeft - 1
+  const countXRight = n - acc.count - accLeft.count - 1
   if (countXRight > target) {
     return true
   }
@@ -58,67 +58,34 @@ const btreeGameWinningMove = (root, n, x) => {
 }
 
 /**
- * Perform DFS Search for X. Returns as soon as the accumulated count exceeds target. Also returns the TreeNode of x if it is found.
+ * Perform DFS Search for X. Returns as soon as the accumulated count exceeds target. Also set acc.nodeX if x is found.
  * @param {*} node
  * @param {*} target
  * @param {*} x
  * @param {*} acc
  */
-const dfsSearchCountWithX = (node, target, x, acc) => {
+const dfsSearchCountNodeX = (node, target, x, acc) => {
   if (!node) {
-    return acc
+    return
   }
 
   if (node.val === x) {
-    return {
-      count: acc.count,
-      nodeX: node
-    }
+    acc.nodeX = node
+    return
   }
 
-  const count = acc.count + 1
-  const nodeX = acc.nodeX
-  const result = { count, nodeX }
+  acc.count += 1
 
-  if (result.count > target) {
-    return result
+  if (acc.count > target) {
+    return
   }
 
-  const resultLeft = dfsSearchCountWithX(node.left, target, x, result)
-  if (resultLeft.count > target) {
-    return resultLeft
+  dfsSearchCountNodeX(node.left, target, x, acc)
+  if (acc.count > target) {
+    return
   }
 
-  const resultLeftRight = dfsSearchCountWithX(node.right, target, x, resultLeft)
-
-  return resultLeftRight
-}
-
-/**
- * Perform DFS search on the TreeNode and returns the node count as soon as it exceeds target.
- * @param {TreeNode} node TreeNode to be searched on.
- * @param {number} target Target count.
- * @param {number} acc Accumulated count.
- * @return {number} Count of nodes. Return as soon as it exceeds target.
- */
-const dfsSearchCount = (node, target, acc) => {
-  if (!node) {
-    return acc
-  }
-
-  const count = acc + 1
-  if (count > target) {
-    return count
-  }
-
-  const countLeft = dfsSearchCount(node.left, target, count)
-  if (countLeft > target) {
-    return countLeft
-  }
-
-  const countLeftRight = dfsSearchCount(node.right, target, countLeft)
-
-  return countLeftRight
+  dfsSearchCountNodeX(node.right, target, x, acc)
 }
 
 module.exports = btreeGameWinningMove
