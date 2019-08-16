25003c510f372305ea506756587c8dc88ecb9c6f
Gan Eng Chin <ecgan@users.noreply.github.com>
2019-08-17 00:25:18 +0800

    remove unused utils.

--- utils/leettree/TreeNode.js
+++
@@ -1,6 +0,0 @@
-function TreeNode (val) {
-  this.val = val
-  this.left = this.right = null
-}
-
-module.exports = TreeNode
--- utils/leettree/index.js
+++
@@ -1,7 +0,0 @@
-const serialize = require('./serialize')
-const deserialize = require('./deserialize')
-
-module.exports = {
-  serialize,
-  deserialize
-}
