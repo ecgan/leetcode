31689c63cf11bd646eaaaf1ee280150808de8ff2
Gan Eng Chin <ecgan@users.noreply.github.com>
2019-08-17 02:52:49 +0800

    added tests with 100% code coverage.

--- problems/binary-tree-coloring-game/solution.test.js
+++ problems/binary-tree-coloring-game/solution.test.js
@@ -1,21 +1,112 @@
-// [1,2,3,4,5,6,7,8,9,10,11]
-// 11
-// 3
-// [1,2,3,4,5]
-// 5
-// 1
-// [6,3,null,7,4,null,null,null,2,null,1,null,5]
-// 7
-// 3
+const leettree = require('leettree')
+const btreeGameWinningMove = require('./solution')
 
-const leettree = require('../../utils/leettree')
+test('Example 1', () => {
+  const root = leettree.deserialize([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
+  const n = 11
+  const x = 3
 
-test('With multiple same contiguous values', () => {
-  const nums = [1, 2, 3, null, null, 4]
+  const result = btreeGameWinningMove(root, n, x)
 
-  const result = leettree.deserialize(nums)
+  expect(result).toBe(true)
+})
 
-  console.log('result: ', result)
+test('x at root, only 1 node, should return false', () => {
+  const root = leettree.deserialize([1])
+  const n = 1
+  const x = 1
 
-  expect(result).toBe(13)
+  const result = btreeGameWinningMove(root, n, x)
+
+  expect(result).toBe(false)
+})
+
+test('x at root, 3 left and 3 right, return false', () => {
+  const root = leettree.deserialize([1, 2, 3, 4, null, 5, null, 6, null, 7])
+  const n = 7
+  const x = 1
+
+  const result = btreeGameWinningMove(root, n, x)
+
+  expect(result).toBe(false)
+})
+
+test('x at root, 4 left and 2 right, return true', () => {
+  const root = leettree.deserialize([1, 2, 3, 4, null, 5, null, 6, null, null, null, 7])
+  const n = 7
+  const x = 1
+
+  const result = btreeGameWinningMove(root, n, x)
+
+  expect(result).toBe(true)
+})
+
+test('x at root, 2 left and 4 right, return true', () => {
+  const root = leettree.deserialize([1, 2, 3, 4, null, 5, null, null, null, 6, null, 7])
+  const n = 7
+  const x = 1
+
+  const result = btreeGameWinningMove(root, n, x)
+
+  expect(result).toBe(true)
+})
+
+test('5 nodes, x in the middle, return false', () => {
+  const root = leettree.deserialize([1, 2, null, 3, null, 4, null, 5])
+  const n = 5
+  const x = 3
+
+  const result = btreeGameWinningMove(root, n, x)
+
+  expect(result).toBe(false)
+})
+
+test('7 nodes, x in the middle, return false', () => {
+  const root = leettree.deserialize([1, 2, null, 3, null, 4, 5, 6, null, null, 7])
+  const n = 7
+  const x = 3
+
+  const result = btreeGameWinningMove(root, n, x)
+
+  expect(result).toBe(false)
+})
+
+test('7 nodes, 2-3-1 branch model, left branch with length 3, return false', () => {
+  const root = leettree.deserialize([1, 2, null, 3, null, 4, 5, 6, null, null, null, 7])
+  const n = 7
+  const x = 3
+
+  const result = btreeGameWinningMove(root, n, x)
+
+  expect(result).toBe(false)
+})
+
+test('7 nodes, 2-1-3 branch model, right branch with length 3, return false', () => {
+  const root = leettree.deserialize([1, 2, null, 3, null, 4, 5, null, null, null, 6, null, 7])
+  const n = 7
+  const x = 3
+
+  const result = btreeGameWinningMove(root, n, x)
+
+  expect(result).toBe(false)
+})
+
+test('7 nodes, 1-4-1 branch model, left branch with length 4, return true', () => {
+  const root = leettree.deserialize([1, 2, null, 3, 4, 5, null, null, null, 6, null, 7])
+  const n = 7
+  const x = 2
+
+  const result = btreeGameWinningMove(root, n, x)
+
+  expect(result).toBe(true)
+})
+
+test('7 nodes, 1-1-4 branch model, right branch with length 4, return true', () => {
+  const root = leettree.deserialize([1, 2, null, 3, 4, null, null, null, 5, null, 6, 7])
+  const n = 7
+  const x = 2
+
+  const result = btreeGameWinningMove(root, n, x)
+
+  expect(result).toBe(true)
 })
