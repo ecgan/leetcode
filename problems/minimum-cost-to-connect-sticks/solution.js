062710546ec10e044a6f3c1eae32ddd8e8967acf
Gan Eng Chin <ecgan@users.noreply.github.com>
2019-08-25 00:10:49 +0800

    code refactor.

--- problems/minimum-cost-to-connect-sticks/solution.js
+++ problems/minimum-cost-to-connect-sticks/solution.js
@@ -1,14 +1,9 @@
 const sortBy = require('lodash/sortBy')
 const sortedIndex = require('lodash/sortedIndex')
 
-const connectSticks = (sticks) => {
-  if (sticks.length === 1) {
-    return 0
-  }
+const getCost = (sorted) => {
+  let cost = 0
 
-  const sorted = sortBy(sticks)
-
-  let result = 0
   while (sorted.length > 1) {
     const first = sorted.shift()
     const second = sorted.shift()
@@ -17,10 +12,20 @@ const connectSticks = (sticks) => {
     const insertIndex = sortedIndex(sorted, combined)
     sorted.splice(insertIndex, 0, combined)
 
-    result += combined
+    cost += combined
   }
 
-  return result
+  return cost
+}
+
+const connectSticks = (sticks) => {
+  if (sticks.length === 1) {
+    return 0
+  }
+
+  const sorted = sortBy(sticks)
+
+  return getCost(sorted)
 }
 
 module.exports = connectSticks
