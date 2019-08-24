6463778d718bac0efa45e41ca373a2f046ff4df8
Gan Eng Chin <ecgan@users.noreply.github.com>
2019-08-25 00:03:52 +0800

    code refactor.

--- problems/single-row-keyboard/solution.js
+++ problems/single-row-keyboard/solution.js
@@ -1,10 +1,15 @@
-const calculateTime = (keyboard, word) => {
+const getMap = (keyboard) => {
   const map = {}
+
   for (let i = 0; i < keyboard.length; i++) {
     const char = keyboard[i]
     map[char] = i
   }
 
+  return map
+}
+
+const getTime = (map, word) => {
   let curPos = 0
   let time = 0
   for (let i = 0; i < word.length; i++) {
@@ -16,6 +21,12 @@ const calculateTime = (keyboard, word) => {
   }
 
   return time
+}
+
+const calculateTime = (keyboard, word) => {
+  const map = getMap(keyboard)
+
+  return getTime(map, word)
 }
 
 module.exports = calculateTime
