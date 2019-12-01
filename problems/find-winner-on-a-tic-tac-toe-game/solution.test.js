b57b1810c609ee992e1767c67feb5b4adb2a3467
Gan Eng Chin <ecgan@users.noreply.github.com>
2019-12-01 20:41:24 +0800

    added more test cases for #1275.

--- problems/find-winner-on-a-tic-tac-toe-game/solution.test.js
+++ problems/find-winner-on-a-tic-tac-toe-game/solution.test.js
@@ -31,3 +31,35 @@ test('Example 4', () => {
 
   expect(result).toStrictEqual('Pending')
 })
+
+test('Player A win in the first row', () => {
+  const moves = [[0, 0], [1, 0], [0, 1], [1, 1], [0, 2]]
+
+  const result = tictactoe(moves)
+
+  expect(result).toStrictEqual('A')
+})
+
+test('Player A win in the third row', () => {
+  const moves = [[2, 0], [1, 0], [2, 1], [1, 1], [2, 2]]
+
+  const result = tictactoe(moves)
+
+  expect(result).toStrictEqual('A')
+})
+
+test('Player A win in the second column', () => {
+  const moves = [[0, 1], [0, 0], [1, 1], [1, 0], [2, 1]]
+
+  const result = tictactoe(moves)
+
+  expect(result).toStrictEqual('A')
+})
+
+test('Player A win in the third column', () => {
+  const moves = [[0, 2], [0, 1], [1, 2], [1, 1], [2, 2]]
+
+  const result = tictactoe(moves)
+
+  expect(result).toStrictEqual('A')
+})
