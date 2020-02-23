bab56ec490c94d125be108ecf63c8a2464be0616
Gan Eng Chin <ecgan@users.noreply.github.com>
2020-02-23 17:27:09 +0800

    added solution for #1361 Validate Binary Tree Nodes.

---
+++ problems/validate-binary-tree-nodes/validateBinaryTreeNodes.test.js
@@ -0,0 +1,71 @@
+const validateBinaryTreeNodes = require('./validateBinaryTreeNodes')
+
+test('Example 1', () => {
+  const n = 4
+  const leftChild = [1, -1, 3, -1]
+  const rightChild = [2, -1, -1, -1]
+
+  const result = validateBinaryTreeNodes(n, leftChild, rightChild)
+
+  expect(result).toBe(true)
+})
+
+test('Example 2', () => {
+  const n = 4
+  const leftChild = [1, -1, 3, -1]
+  const rightChild = [2, 3, -1, -1]
+
+  const result = validateBinaryTreeNodes(n, leftChild, rightChild)
+
+  expect(result).toBe(false)
+})
+
+test('Example 3', () => {
+  const n = 2
+  const leftChild = [1, 0]
+  const rightChild = [-1, -1]
+
+  const result = validateBinaryTreeNodes(n, leftChild, rightChild)
+
+  expect(result).toBe(false)
+})
+
+test('Example 4', () => {
+  const n = 6
+  const leftChild = [1, -1, -1, 4, -1, -1]
+  const rightChild = [2, -1, -1, 5, -1, -1]
+
+  const result = validateBinaryTreeNodes(n, leftChild, rightChild)
+
+  expect(result).toBe(false)
+})
+
+test('only one node, no child, should return true', () => {
+  const n = 1
+  const leftChild = [-1]
+  const rightChild = [-1]
+
+  const result = validateBinaryTreeNodes(n, leftChild, rightChild)
+
+  expect(result).toBe(true)
+})
+
+test('three nodes, first node has no child, should return false', () => {
+  const n = 3
+  const leftChild = [-1, -1, -1]
+  const rightChild = [-1, 2, -1]
+
+  const result = validateBinaryTreeNodes(n, leftChild, rightChild)
+
+  expect(result).toBe(false)
+})
+
+test('three nodes, first node has two children, should return true', () => {
+  const n = 3
+  const leftChild = [1, -1, -1]
+  const rightChild = [2, -1, -1]
+
+  const result = validateBinaryTreeNodes(n, leftChild, rightChild)
+
+  expect(result).toBe(true)
+})
