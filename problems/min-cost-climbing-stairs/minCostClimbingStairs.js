e1bd9fe1c5e2156aab8fd70eb8b7b35f45db4a51
Gan Eng Chin <ecgan@users.noreply.github.com>
2020-03-02 22:27:15 +0800

    code coverage.

--- problems/min-cost-climbing-stairs/minCostClimbingStairs.js
+++ problems/min-cost-climbing-stairs/minCostClimbingStairs.js
@@ -8,18 +8,18 @@ const getCalculateCost = (cost, stepMinCostMap) => {
       return Math.min(cost[0], cost[1])
     }
 
-    if (!stepMinCostMap.has(dest - 1)) {
-      stepMinCostMap.set(dest - 1, calculateCost(dest - 1))
-    }
-
     if (!stepMinCostMap.has(dest - 2)) {
       stepMinCostMap.set(dest - 2, calculateCost(dest - 2))
     }
 
-    const last1step = stepMinCostMap.get(dest - 1) + cost[dest - 1]
-    const last2step = stepMinCostMap.get(dest - 2) + cost[dest - 2]
+    if (!stepMinCostMap.has(dest - 1)) {
+      stepMinCostMap.set(dest - 1, calculateCost(dest - 1))
+    }
 
-    const result = Math.min(last1step, last2step)
+    const last2step = stepMinCostMap.get(dest - 2) + cost[dest - 2]
+    const last1step = stepMinCostMap.get(dest - 1) + cost[dest - 1]
+
+    const result = Math.min(last2step, last1step)
 
     return result
   }
