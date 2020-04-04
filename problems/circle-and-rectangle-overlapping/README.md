0c7a330176680c85b5b2fd0ae7b1638ed707eb05
Gan Eng Chin <ecgan@users.noreply.github.com>
2020-04-05 01:48:13 +0800

    add solution for #1401 Circle and Rectangle Overlapping.

---
+++ problems/circle-and-rectangle-overlapping/README.md
@@ -0,0 +1,53 @@
+# Circle and Rectangle Overlapping
+
+LeetCode #: [1401](https://leetcode.com/problems/circle-and-rectangle-overlapping/)
+
+Difficulty: Medium
+
+Topics: Geometry.
+
+## Problem
+
+*Note: Visit LeetCode page for illustrations.*
+
+Given a circle represented as (`radius`, `x_center`, `y_center`) and an axis-aligned rectangle represented as (`x1`, `y1`, `x2`, `y2`), where (`x1`, `y1`) are the coordinates of the bottom-left corner, and (`x2`, `y2`) are the coordinates of the top-right corner of the rectangle.
+
+Return True if the circle and rectangle are overlapped otherwise return False.
+
+In other words, check if there are any point (xi, yi) such that belongs to the circle and the rectangle at the same time.
+
+Example 1:
+
+```text
+Input: radius = 1, x_center = 0, y_center = 0, x1 = 1, y1 = -1, x2 = 3, y2 = 1
+Output: true
+Explanation: Circle and rectangle share the point (1,0)
+```
+
+Example 2:
+
+```text
+Input: radius = 1, x_center = 0, y_center = 0, x1 = -1, y1 = 0, x2 = 0, y2 = 1
+Output: true
+```
+
+Example 3:
+
+```text
+Input: radius = 1, x_center = 1, y_center = 1, x1 = -3, y1 = -3, x2 = 3, y2 = 3
+Output: true
+```
+
+Example 4:
+
+```text
+Input: radius = 1, x_center = 1, y_center = 1, x1 = 1, y1 = -3, x2 = 2, y2 = -1
+Output: false
+```
+
+Constraints:
+
+* `1 <= radius <= 2000`
+* `-10^4 <= x_center, y_center, x1, y1, x2, y2 <= 10^4`
+* `x1 < x2`
+* `y1 < y2`
---
+++ problems/circle-and-rectangle-overlapping/checkOverlap.js
@@ -0,0 +1,106 @@
+/* eslint-disable camelcase */
+const getLength = (point1, point2) => {
+  const [x1, y1] = point1
+  const [x2, y2] = point2
+
+  const diffX = x1 - x2
+  const diffY = y1 - y2
+
+  return Math.hypot(diffX, diffY)
+}
+
+const isOverlapVerticalEdge = (circle, corner1, corner2) => {
+  const [x_center, y_center, radius] = circle
+  const [x1, y1] = corner1
+  const [, y2] = corner2
+
+  if (x_center + radius >= x1 && x_center - radius <= x1) {
+    if (y_center >= y1 && y_center <= y2) {
+      return true
+    }
+  }
+
+  return false
+}
+
+const isOverlapHorizontalEdge = (circle, corner1, corner2) => {
+  const [x_center, y_center, radius] = circle
+  const [x1, y1] = corner1
+  const [x2] = corner2
+
+  if (y_center + radius >= y1 && y_center - radius <= y1) {
+    if (x_center >= x1 && x_center <= x2) {
+      return true
+    }
+  }
+
+  return false
+}
+
+const isOverlapCorner = (circle, corner) => {
+  const [x_center, y_center, radius] = circle
+
+  const pointCircle = [x_center, y_center]
+  const length = getLength(pointCircle, corner)
+
+  if (length <= radius) {
+    return true
+  }
+
+  return false
+}
+
+const isCircleInRect = (circle, lowerLeft, upperRight) => {
+  const [x_center, y_center] = circle
+  const [x1, y1] = lowerLeft
+  const [x2, y2] = upperRight
+
+  return (
+    x_center >= x1 && x_center <= x2 &&
+    y_center >= y1 && y_center <= y2
+  )
+}
+
+/**
+ * @param {number} radius
+ * @param {number} x_center
+ * @param {number} y_center
+ * @param {number} x1
+ * @param {number} y1
+ * @param {number} x2
+ * @param {number} y2
+ * @return {boolean}
+ */
+const checkOverlap = function (radius, x_center, y_center, x1, y1, x2, y2) {
+  const circle = [x_center, y_center, radius]
+  const lowerLeft = [x1, y1]
+  const upperLeft = [x1, y2]
+  const upperRight = [x2, y2]
+  const lowerRight = [x2, y1]
+
+  if (
+    isOverlapCorner(circle, lowerLeft) ||
+    isOverlapCorner(circle, upperLeft) ||
+    isOverlapCorner(circle, upperRight) ||
+    isOverlapCorner(circle, lowerRight)
+  ) {
+    return true
+  }
+
+  if (
+    isOverlapVerticalEdge(circle, lowerLeft, upperLeft) ||
+    isOverlapVerticalEdge(circle, lowerRight, upperRight) ||
+    isOverlapHorizontalEdge(circle, lowerLeft, lowerRight) ||
+    isOverlapHorizontalEdge(circle, upperLeft, upperRight)
+  ) {
+    return true
+  }
+
+  if (isCircleInRect(circle, lowerLeft, upperRight)) {
+    return true
+  }
+
+  return false
+}
+
+module.exports = checkOverlap
