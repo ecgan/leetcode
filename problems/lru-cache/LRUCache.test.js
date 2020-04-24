42a14b82b346da5c430286516a93814f56cb2c58
Gan Eng Chin <ecgan@users.noreply.github.com>
2020-04-24 17:23:54 +0800

    add test for code coverage in #146.

--- problems/lru-cache/LRUCache.test.js
+++ problems/lru-cache/LRUCache.test.js
@@ -25,3 +25,23 @@ test('Example 1', () => {
   const e = cache.get(4) // returns 4
   expect(e).toBe(4)
 })
+
+test('put multiple times with same key different value', () => {
+  const cache = new LRUCache(2)
+
+  cache.put(1, 1)
+  cache.put(2, 2)
+  cache.put(3, 3)
+
+  const a = cache.get(1)
+  expect(a).toBe(-1)
+
+  cache.put(2, 10)
+  cache.put(2, 100)
+
+  const b = cache.get(2)
+  expect(b).toBe(100)
+
+  const c = cache.get(3)
+  expect(c).toBe(3)
+})
