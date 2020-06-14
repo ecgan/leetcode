556d3e98049ca65801e92bf4496e0729548a92f1
Gan Eng Chin <ecgan@users.noreply.github.com>
2020-06-14 19:30:17 +0800

    add tests for #787 code coverage.

--- problems/cheapest-flights-within-k-stops/findCheapestPrice.test.js
+++ problems/cheapest-flights-within-k-stops/findCheapestPrice.test.js
@@ -23,3 +23,27 @@ test('Example 2', () => {
 
   expect(result).toBe(500)
 })
+
+test('no such route should return -1', () => {
+  const n = 4
+  const flights = [[0, 1, 100], [1, 2, 100], [0, 2, 500], [2, 3, 200]]
+  const src = 0
+  const dst = 3
+  const k = 0
+
+  const result = findCheapestPrice(n, flights, src, dst, k)
+
+  expect(result).toBe(-1)
+})
+
+test('intermediate city 4 has no flight out', () => {
+  const n = 4
+  const flights = [[0, 1, 100], [1, 4, 120], [0, 2, 500], [2, 3, 200]]
+  const src = 0
+  const dst = 3
+  const k = 5
+
+  const result = findCheapestPrice(n, flights, src, dst, k)
+
+  expect(result).toBe(700)
+})
