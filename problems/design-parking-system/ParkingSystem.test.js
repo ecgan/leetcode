e665a2f39226a3334210055b37e953f722d19da3
Gan Eng Chin <ecgan@users.noreply.github.com>
2020-10-04 02:13:08 +0800

    add more tests for #1603.

--- problems/design-parking-system/ParkingSystem.test.js
+++ problems/design-parking-system/ParkingSystem.test.js
@@ -8,3 +8,27 @@ test('Example 1', () => {
   expect(parkingSystem.addCar(3)).toBe(false)
   expect(parkingSystem.addCar(1)).toBe(false)
 })
+
+test('Adding big car til no more space', () => {
+  const parkingSystem = new ParkingSystem(2, 0, 0)
+
+  expect(parkingSystem.addCar(1)).toBe(true)
+  expect(parkingSystem.addCar(1)).toBe(true)
+  expect(parkingSystem.addCar(1)).toBe(false)
+})
+
+test('Adding medium car til no more space', () => {
+  const parkingSystem = new ParkingSystem(0, 2, 0)
+
+  expect(parkingSystem.addCar(2)).toBe(true)
+  expect(parkingSystem.addCar(2)).toBe(true)
+  expect(parkingSystem.addCar(2)).toBe(false)
+})
+
+test('Adding small car til no more space', () => {
+  const parkingSystem = new ParkingSystem(0, 0, 2)
+
+  expect(parkingSystem.addCar(3)).toBe(true)
+  expect(parkingSystem.addCar(3)).toBe(true)
+  expect(parkingSystem.addCar(3)).toBe(false)
+})
