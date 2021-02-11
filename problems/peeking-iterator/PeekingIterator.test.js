16826112443494182e2d78dde98bbd82905d678e
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-02-12 00:30:25 +0800

    Add more test case for #284 Peeking Iterator.

--- problems/peeking-iterator/PeekingIterator.test.js
+++ problems/peeking-iterator/PeekingIterator.test.js
@@ -11,3 +11,15 @@ test('Example 1', () => {
   expect(peekingIterator.next()).toBe(3)
   expect(peekingIterator.hasNext()).toBe(false)
 })
+
+test('Multiple peeks', () => {
+  const arr = [1, 2, 3]
+  const peekingIterator = new PeekingIterator(new Iterator(arr))
+
+  expect(peekingIterator.next()).toBe(1)
+  expect(peekingIterator.peek()).toBe(2)
+  expect(peekingIterator.peek()).toBe(2)
+  expect(peekingIterator.next()).toBe(2)
+  expect(peekingIterator.next()).toBe(3)
+  expect(peekingIterator.hasNext()).toBe(false)
+})
